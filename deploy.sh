f3f0e868175a865642b0ca9cb6d548a05b7cc05c
hpx <m15257990085@163.com>
2019-02-28 09:41:50 +0800

    h

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 kill -9 $(pgrep webserver)
-cd ~/auto-deploy
-git pull git@github.com:pengxianghu/auto-deploy.git
-cd webserver
+git pull https://github.com/pengxianghu/auto-deploy.git
+cd ~/auto-deploy/webserver
 ./webserver &
